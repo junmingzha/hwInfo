257e2385fdc7c995cc1c64be01a62a7c0ac6ab4d
xinchen.luan <xinchen.luan@transwarp.io>
2020-08-12 16:14:45 +0800

    增加内存容量和频率检测

--- hwinfo.sh
+++ hwinfo.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#Linux Server Hardware Configuration Collector
+#Server Hardware Configuration Collector
+#For RHEL7/CentOS7
 #Created by xinchen.luan
 #Date:08/04/2020
 if [ $UID != 0 ]; then
@@ -7,6 +8,7 @@ if [ $UID != 0 ]; then
     exit 1
 fi
 
+#Format STo
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 PLAIN='\033[0m'
@@ -35,7 +37,7 @@ install_lspci(){
     if [ $? -eq 0 ];then
         echo -e "${GREEN} [OK] lspci is ready.${PLAIN}"
     else
-        echo -e "${RED} [FAILED] lspci installation failed, please install manually.${PLAIN}"
+        echo -e "${RED} [FAILED] lspci installation failed, please try to install manually.${PLAIN}"
         exit 1
     fi
 }
@@ -45,19 +47,25 @@ install_dmi(){
     if [ $? -eq 0 ];then
         echo -e "${GREEN} [OK] dmidecode is ready.${PLAIN}"
     else
-        echo -e "${RED} [FAILED] dmidecode installation failed, please install manually.${PLAIN}"
+        echo -e "${RED} [FAILED] dmidecode installation failed, please try to install manually.${PLAIN}"
         exit 1
     fi
 }
 
-
 getSYSinf(){
+    #System Manufacturer
     SYSmaf=`dmidecode |grep -A16 "System Information$" |grep "Manufacturer"|head -n 1 |awk -F': ' '{print $2}'`
+    #System Model Name
     SYSmod=`dmidecode |grep -A16 "System Information$" |grep "Product Name"|head -n 1|awk -F': ' '{print $2}'`
+    #System Serial Number (by product,not node)
     SYSsn=`dmidecode |grep -A16 "System Information$" |grep "Serial Number"|head -n 1|awk -F': ' '{print $2}'`
+    #Hostname
     SYSnm=`hostname -f`
+    #OS Version
     SYSos=`cat /etc/system-release`
+    #Kernel Version
     SYSkn=`uname -r`
+    #System IP Address
     SYSip=`ip addr | grep 'state UP' -A2 | grep '172.[12]6' | head -n 1 | awk '{print $2}' | cut -f1 -d '/'`
     
     echo $SYSmaf
@@ -70,30 +78,78 @@ getSYSinf(){
 }
 
 getCPUinf(){
+    #CPU Model Name
     CPUnm=`cat /proc/cpuinfo | grep "model name" | uniq |awk -F': ' '{print $2}'`
+    #CPUs Count
     CPUcut=`cat /proc/cpuinfo| grep "physical id"| sort| uniq| wc -l`
+    #Cores Count (Per CPU)
     CPUcore=`cat /proc/cpuinfo | grep "core id" | sort | uniq | wc -l`
+    #Threads Count
     CPUproc=`cat /proc/cpuinfo| grep "processor"| wc -l`
-    CPUhz=
+
+    echo $CPUcore
+    echo $CPUcut
+    echo $CPUnm
+    echo $CPUproc
 }
 
 getMEMinf(){
+    #Memory Total Size 
     MEMtotal=`dmidecode -t memory | grep  Size: | grep -v "No Module Installed" | awk '{sum+=$2}END{print sum,$3}'`
+    #Memory Slot Count
     MEMsltcut=`dmidecode|grep -P -A5 "Memory\s+Device"|grep Size|grep -v Range|wc -l`
+    #Uesd Memory Slot Count
     MEMsltuse=`dmidecode -t memory | grep Size | grep -v "No Module Installed"|wc -l`
-    MEMhz=`dmidecode -t memory | grep "Speed:" |grep -v "Unknown"`
+    #Memory Speed (Standard Memory Speed , not Clock Speed)
+    MEMhz=`dmidecode -t memory | grep "Speed:" |grep -v "Unknown"|grep -v "Clock Speed:"|awk -F': ' '{print $2}'|sed 's/[ ][ ]*//g'`
+
+    #Memory Size (Per Slot)
+    slotsize=0
+    for i in `dmidecode -t memory | grep  Size: | grep -v "Installed" |awk -F': ' '{print $2}'|sed 's/[ ][ ]*//g'`
+    do
+        MEMsize[$slotsize]=$i
+        ((slotsize++))
+    done
+
+    #Memory Speed (Per Slot)
+    slotspd=0
+    for s in `dmidecode -t memory | grep "Speed:" |grep -v "Unknown"|grep -v "Clock Speed:"|awk -F': ' '{print $2}'|sed 's/[ ][ ]*//g'`
+    do
+        MEMspd[$slotspd]=$s
+        ((slotspd++))
+    done
+
+    echo $MEMtotal
+    echo $MEMsltcut
+    echo $MEMsltuse
+    echo $MEMhz
+
+    #Echo Memory Size & Speed
+    m=0
+    while [ $m -lt $m1 ]
+    do
+        echo "Slot$m Size: ${MEMsize[$m]}"
+        echo "Slot$m Speed: ${MEMspd[$m]}"
+        ((m++))
+    done
 }
 
 getDISKinf(){
+    #Disk Count (SATA , SAS or NVMe Device)
     DISKcut=`fdisk -l |grep 'Disk /dev/sd*' |awk -F , '{print $1}' | sed 's/Disk identifier.*//g' | sed '/^$/d'|wc -l`
     DISKsltc=
     DISKsz=
+
+    echo $DISKcut
 }
 
 getETHinf(){
     ETHcut=`lspci |grep -i ethernet |wc -l`
     ETHmod=`lspci |grep -i ethernet |awk -F': ' '{print $2}'`
     ETHsp=
+
+    echo $ETHcut
+    echo $ETHmod
 }
 
 chk_lspci
